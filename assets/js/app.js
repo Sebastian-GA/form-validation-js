4faeb811afe7a3a286aad9b507bf7fa6c5baa8a6
Sebastian GA <isebastian.ga@gmail.com>
2023-03-20 15:55:26 -0500

    custom error messages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,8 @@
-import { validations } from "./validations.js";
+import { validate } from "./validations.js";
 
 const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
-    const inputType = input.dataset.type;
-
-    // If exist validations for this input type
-    if (!(validations[inputType] === undefined)) {
-        input.addEventListener("blur", (input) => {
-            validations[inputType](input.target);
-        });
-    }
+    input.addEventListener("blur", (input) => {
+        validate(input.target);
+    });
 });
--- assets/js/validations.js
+++ assets/js/validations.js
@@ -1,7 +1,59 @@
-export const validations = {
+export function validate(input) {
+    const inputType = input.dataset.type;
+
+    // If exist a validation function for this input type
+    if (!(validationFunctions[inputType] === undefined)) {
+        validationFunctions[inputType](input);
+    }
+    
+    // If input not valid then add class "input-container--invalid"
+    if (!input.validity.valid) {
+        input.parentElement.classList.add("input-container--invalid");
+        
+        // If exist a custom validation error message for this input type
+        if (!(validationErrors[inputType] === undefined)) {
+            // For each error type for this input type
+            Object.keys(validationErrors[inputType]).forEach((error) => {
+                if (input.validity[error]) {
+                    input.parentElement.querySelector(".input-message-error").textContent = validationErrors[inputType][error];
+                    /* break; */ // forEach loops cant be stopped with break :( It's like a function
+                    return; // return is the way to stop a forEach loop
+                }
+            });
+        }
+    } else {
+        input.parentElement.classList.remove("input-container--invalid");
+    }
+}
+
+const validationFunctions = {
     birth: (input) => validateBirth(input),
 };
 
+const validationErrors = {
+    name: {
+        valueMissing: "Debes ingresar un nombre",
+    },
+    email: {
+        valueMissing: "Debes ingresar un correo electrónico",
+        typeMismatch: "Debes ingresar un correo electrónico válido",
+    },
+    password: {
+        valueMissing: "Debes ingresar una contraseña",
+        patternMismatch:
+            "Debe contener al menos 6 caractéres, máximo 12. Debe incluir al menos una letra minúscula, una mayúscula, un número y no debe incluir caractéres especiales",
+    },
+    birth: {
+        valueMissing: "Debes ingresar tu fecha de nacimiento",
+        customError: "Debes ser mayor de 18 años",
+    },
+    phoneNumber: {
+        valueMissing: "Debes ingresar un número de teléfono",
+        patternMismatch: "Debes ingresar un número de teléfono válido",
+    },
+};
+
+// --------- Validate birth date --------- //
 function validateBirth(input) {
     const birthDateValue = new Date(input.value);
     if (!isAdult(birthDateValue)) {
